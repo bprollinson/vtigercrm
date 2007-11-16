6f56ae7f0845f80cd0136b8e8a6adda0a9c865c4
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-11-16 12:36:45 +0000

    Tagged  - SQL Injection Patch.

---
+++ vtigercrm/tags/vtigercrm-5.0.4-rc3/Smarty/templates/DetailViewFields.tpl
@@ -0,0 +1,107 @@
+{*<!--
+
+/*********************************************************************************
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+*
+ ********************************************************************************/
+
+-->*}
+
+<!-- This file is used to display the fields based on the ui type in detailview -->
+		{if $keyid eq '1' || $keyid eq 2 || $keyid eq '11' || $keyid eq '7' || $keyid eq '9' || $keyid eq '55' || $keyid eq '71' || $keyid eq '72' || $keyid eq '255'} <!--TextBox-->
+                                         		<td width=25% class="dvtCellInfo" align="left">&nbsp;
+                                         		        {if $keyid eq '55' || $keyid eq '255'}<!--SalutationSymbol-->
+									{if $keyaccess eq $APP.LBL_NOT_ACCESSIBLE}
+										{$APP.LBL_NOT_ACCESSIBLE}
+									{else}
+                                                                		{$keysalut}
+									{/if}
+								{*elseif $keyid eq '71' || $keyid eq '72'}  <!--CurrencySymbol-->
+                                                                    {$keycursymb*}
+                                                                {/if}
+                                                       {$keyval}
+                                                  </td>
+                                             {elseif $keyid eq '13'} <!--Email-->
+                                                  <td width=25% class="dvtCellInfo" align="left">
+							{if $smarty.session.internal_mailer eq 1}
+                                                                <a href="javascript:InternalMailer({$ID},{$keyfldid},'{$keyfldname}','{$MODULE}','record_id');window.close();">{$keyval}</a>
+                                                        {else}
+                                                                <a href="mailto:{$keyval}" target="_blank" >{$keyval}</a>
+                                                        {/if}
+                                                        </span>
+						  </td>
+                                             {elseif $keyid eq '15' || $keyid eq '16' || $keyid eq '111'} <!--ComboBox-->
+               					<td width=25% class="dvtCellInfo" align="left">&nbsp;
+							{foreach item=arr from=$keyoptions}
+                                                        {if $arr[0] eq $APP.LBL_NOT_ACCESSIBLE}
+                                                                {assign var=keyval value=$APP.LBL_NOT_ACCESSIBLE}
+                                                        {/if}
+							{/foreach}
+							{$keyval}
+               					</td>
+                                             {elseif $keyid eq '17'} <!--WebSite-->
+                                                  <td width=25% class="dvtCellInfo" align="left">&nbsp;<a href="http://{$keyval}" target="_blank">{$keyval}</a>
+                                                  </td>
+                                             {elseif $keyid eq '19' || $keyid eq '20'} <!--TextArea/Description-->
+                                                  <td width=100% class="dvtCellInfo" align="left">&nbsp;{$keyval}                   
+                                                  </td>
+                                             {elseif $keyid eq '21' || $keyid eq '24' || $keyid eq '22'} <!--TextArea/Street-->
+                                                  <td width=25% class="dvtCellInfo" align="left">&nbsp;{$keyval}
+                                                  </td>
+                                             {elseif $keyid eq '50' || $keyid eq '73' || $keyid eq '51' || $keyid eq '57' || $keyid eq '59' || $keyid eq '75' || $keyid eq '81' || $keyid eq '76' || $keyid eq '78' || $keyid eq '80'} <!--AccountPopup-->
+                                                  <td width=25% class="dvtCellInfo" align="left">&nbsp;<a href="{$keyseclink}">{$keyval}</a>
+                                                  </td>
+                                             {elseif $keyid eq 82} <!--Email Body-->
+                                                  <td colspan="3" width=100% class="dvtCellInfo" align="left">&nbsp;{$keyval}
+                                                  </td>
+					{elseif $keyid eq '53'} <!--Assigned To-->
+                    <td width=25% class="dvtCellInfo" align="left">&nbsp;
+                    {if $keyseclink eq ''}
+                        {$keyval}
+                    {else}
+                        <a href="{$keyseclink}">{$keyval}</a>         
+                    {/if}
+					&nbsp;
+                    
+                    </td>
+		    {elseif $keyid eq '56'} <!--CheckBox--> 
+                      <td width=25% class="dvtCellInfo" align="left">{$keyval}&nbsp;
+                        </td>     
+		{elseif $keyid eq 83}<!-- Handle the Tax in Inventory -->
+							<td align="right" class="dvtCellLabel">
+							{$APP.LBL_VAT} {$APP.COVERED_PERCENTAGE}
+							
+							</td>
+							<td class="dvtCellInfo" align="left">&nbsp;
+							{$VAT_TAX}
+							</td>
+							<td colspan="2" class="dvtCellInfo">&nbsp;</td>
+						   </tr>
+		   				   <tr>
+							<td align="right" class="dvtCellLabel">
+							{$APP.LBL_SALES} {$APP.LBL_TAX} {$APP.COVERED_PERCENTAGE}
+							</td> 
+							<td class="dvtCellInfo" align="left">&nbsp;
+								{$SALES_TAX}
+							</td>	
+							<td colspan="2" class="dvtCellInfo">&nbsp;</td>
+						   </tr>
+				   		   <tr>
+							<td align="right" class="dvtCellLabel">
+								{$APP.LBL_SERVICE} {$APP.LBL_TAX} {$APP.COVERED_PERCENTAGE}
+							</td>
+							<td class="dvtCellInfo" align="left" >&nbsp;
+								{$SERVICE_TAX}
+							</td>
+	
+
+				{elseif $keyid eq 69}<!-- for Image Reflection -->
+                                                  	<td align="left" width=25%">&nbsp;{$keyval}</td>
+				{else}									
+                                                  	<td class="dvtCellInfo" align="left" width=25%">&nbsp;{$keyval}</td>
+				{/if}
---
+++ vtigercrm/tags/vtigercrm-5.0.4-rc3/include/utils/export.php
@@ -0,0 +1,212 @@
+<?php
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
+ * ("License"); You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * The Original Code is:  SugarCRM Open Source
+ * The Initial Developer of the Original Code is SugarCRM, Inc.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ ********************************************************************************/
+
+if (substr(phpversion(), 0, 1) == "5") {
+        ini_set("zend.ze1_compatibility_mode", "1");
+}
+
+require_once('config.php');
+require_once('include/logging.php');
+require_once('include/database/PearDatabase.php');
+require_once('modules/Accounts/Accounts.php');
+require_once('modules/Contacts/Contacts.php');
+require_once('modules/Leads/Leads.php');
+require_once('modules/Contacts/Contacts.php');
+require_once('modules/Emails/Emails.php');
+require_once('modules/Calendar/Activity.php');
+require_once('modules/Notes/Notes.php');
+require_once('modules/Potentials/Potentials.php');
+require_once('modules/Users/Users.php');
+require_once('modules/Products/Products.php');
+require_once('include/utils/UserInfoUtil.php');
+
+global $allow_exports,$app_strings;
+
+session_start();
+
+$current_user = new Users();
+
+if(isset($_SESSION['authenticated_user_id']))
+{
+        $result = $current_user->retrieve_entity_info($_SESSION['authenticated_user_id'],"Users");
+        if($result == null)
+        {
+		session_destroy();
+		header("Location: index.php?action=Login&module=Users");
+        }
+
+}
+
+//Security Check
+if(isPermitted($_REQUEST['module'],"Export") == "no")
+{
+	$allow_exports="none";
+}
+
+if ($allow_exports=='none' || ( $allow_exports=='admin' && ! is_admin($current_user) ) )
+{
+
+?>
+	<script language=javascript>
+		alert("<?php echo $app_strings['NOT_PERMITTED_TO_EXPORT']?>");
+		window.location="index.php?module=<?php echo $_REQUEST['module'] ?>&action=index";
+	</script>
+<?php
+}
+
+/**Function convert line breaks to space in description during export 
+ * Pram $str - text
+ * retrun type string
+*/
+function br2nl_vt($str) 
+{
+	global $log;
+	$log->debug("Entering br2nl_vt(".$str.") method ...");
+	$str = preg_replace("/(\r\n)/", " ", $str);
+	$log->debug("Exiting br2nl_vt method ...");
+	return $str;
+}
+
+/**This function exports all the data for a given module
+ * Param $type - module name
+ * Return type text
+*/
+function export($type)
+{
+        global $log,$list_max_entries_per_page;
+        $log->debug("Entering export(".$type.") method ...");
+        global $adb;
+
+        $focus = 0;
+        $content = '';
+
+        if ($type != "")
+        {
+                $focus = new $type;
+        }
+
+        $log = LoggerManager::getLogger('export_'.$type);
+        $db = new PearDatabase();
+	$tablename = $focus->table_name;
+	//$orderby = $focus->getOrderBy();
+	$orderby = $focus->default_order_by;
+	$sorder = $focus->getSortOrder();
+        $order_by = "";
+        //$query = $focus->create_export_query($orderby,$where,$sorder,$tablename);
+        $query = $focus->create_export_query($order_by,$where);
+		$params = array();
+
+        $search_type = $_REQUEST['search_type'];
+        $export_data = $_REQUEST['export_data'];
+        $customview  = $_REQUEST['customview'];
+        $module = $_REQUEST['module'];
+
+        if($search_type == 'withoutsearch' && $export_data == 'all')
+        {
+                $query .= " ORDER BY  ".$tablename.".".$orderby."  ".$sorder;
+        }
+        elseif($search_type == 'withoutsearch' && $export_data == 'currentpage')
+        {
+                if($orderby != '')
+                	$query .=" ORDER BY  ".$tablename.".".$orderby."  ".$sorder." LIMIT  " . ($_SESSION['nav_start']-1) . "," . $list_max_entries_per_page;
+                else
+                    $query .=" LIMIT  " . ($_SESSION['nav_start']-1) . "," . $list_max_entries_per_page;
+	}
+        elseif(($search_type == 'withoutsearch' || $search_type == 'includesearch')&& $export_data == 'selecteddata')
+        {
+                $idstring = explode(",", $_REQUEST['idstring']);
+                if($module == 'Accounts' && count($idstring) > 0) {
+                        $query .= ' and vtiger_account.accountid in ('. generateQuestionMarks($idstring) .')';
+						array_push($params, $idstring);
+				} elseif($module == 'Contacts' && count($idstring) > 0) {
+                        $query .= ' and vtiger_contactdetails.contactid in ('. generateQuestionMarks($idstring) .')';
+						array_push($params, $idstring);
+				} elseif($module == 'Potentials' && count($idstring) > 0) {
+                        $query .= ' and vtiger_potential.potentialid in ('. generateQuestionMarks($idstring) .')';
+						array_push($params, $idstring);
+				} elseif($module == 'Leads' && count($idstring) > 0) {
+                        $query .= ' and vtiger_leaddetails.leadid in ('. generateQuestionMarks($idstring) .')';
+						array_push($params, $idstring);
+				} elseif($module == 'Products' && count($idstring) > 0) {
+                        $query .= ' and vtiger_products.productid in ('. generateQuestionMarks($idstring) .')';
+						array_push($params, $idstring);
+				} elseif($module == 'Notes' && count($idstring) > 0) {
+		        $query .= ' and vtiger_notes.notesid in ('. generateQuestionMarks($idstring) .')';
+						array_push($params, $idstring);
+	       			}		
+        }
+        elseif($search_type == 'includesearch' && $export_data == 'all')
+        {
+                if($orderby != '' && $_SESSION['export_where'] != '') 
+                	$query.=' and  '.$_SESSION['export_where']."  ORDER BY  ".$tablename.".".$orderby."  ".$sorder;
+                elseif($orderby == '' && $_SESSION['export_where'] != '')
+                	$query.=' and  '.$_SESSION['export_where'];
+		else
+			$query .= " ORDER BY  ".$tablename.".".$orderby."  ".$sorder;
+
+        }
+        elseif($search_type == 'includesearch' && $export_data == 'currentpage')
+        {
+		if($orderby != '' && $_SESSION['export_where'] != '')
+                      $query .= "  and  ".$_SESSION['export_where']." ORDER BY  ".$tablename.".".$orderby."  ".$sorder. ' LIMIT '.($_SESSION['nav_start']-1).','.$list_max_entries_per_page;
+                else
+                      $query .=" ORDER BY  ".$tablename.".".$orderby."  ".$sorder.' LIMIT  '.($_SESSION['nav_start']-1).','.$list_max_entries_per_page;
+        }
+
+        $result = $adb->pquery($query, $params, true, "Error exporting $type: "."<BR>$query");
+        $fields_array = $adb->getFieldsArray($result);
+
+	$header = implode("\",\"",array_values($fields_array));
+	$header = "\"" .$header;
+	$header .= "\"\r\n";
+	$content .= $header;
+
+	$column_list = implode(",",array_values($fields_array));
+
+        while($val = $adb->fetchByAssoc($result, -1, false))
+	{
+		$new_arr = array();
+
+		foreach ($val as $key => $value)
+		{
+			if($key=="description" || $key=="note")
+			{
+				$value=br2nl_vt($value);
+			}
+			$value = preg_replace("/(<\/?)(\w+)([^>]*>)/i","",html_entity_decode($value, ENT_QUOTES, "UTF-8"));
+			array_push($new_arr, preg_replace("/\"/","\"\"",$value));
+		}
+		$line = implode("\",\"",$new_arr);
+		$line = "\"" .$line;
+		$line .= "\"\r\n";
+		$content .= $line;
+	}
+	$log->debug("Exiting export method ...");
+	return $content;
+	
+}
+
+$content = export($_REQUEST['module']);
+
+header("Content-Disposition: attachment; filename={$_REQUEST['module']}.csv");
+header("Content-Type: text/csv; charset=UTF-8");
+header( "Expires: Mon, 26 Jul 1997 05:00:00 GMT" );
+header( "Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT" );
+header( "Cache-Control: post-check=0, pre-check=0", false );
+header("Content-Length: ".strlen($content));
+print $content;
+
+exit;
+?>

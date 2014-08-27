b9c01ebc7cafb6be9d1115f5884469e145a86148
prasad <prasad@83ef7b00-670c-0410-b3e3-e45103aa6900>
2014-08-27 12:30:43 +0000

    Tagged 6.1.0 ea (early access) build.

---
+++ vtigercrm/tags/vtigercrm-6.1.0-ea/pkg/vtiger/translations/MexicanSpanishLanguagePack_es_mx/modules/Settings/ModuleManager.php
@@ -0,0 +1,74 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$languageStrings = array(
+	'LBL_IMPORT_MODULE'            => 'Import Module'               , // TODO: Review
+	'LBL_VTIGER_EXTENSION_STORE'   => 'Vtiger Extension Store'      , // TODO: Review
+	'LBL_PUBLISHER'                => 'Publisher'                   , // TODO: Review
+	'LBL_LICENSE'                  => 'License'                     , // TODO: Review
+	'LBL_PUBLISHED_ON'             => 'Published on'                , // TODO: Review
+	'LBL_INSTALL'                  => 'Install'                     , // TODO: Review
+	'LBL_UPGRADE'                  => 'Upgrade'                     , // TODO: Review
+	'LBL_VERSION'                  => 'Version'                     , // TODO: Review
+	'LBL_DECLINE'                  => 'Decline'                     , // TODO: Review
+	'LBL_ACCEPT_AND_INSTALL'       => 'Accept and Install'          , // TODO: Review
+	'LBL_ALREADY_EXISTS'           => 'Already Exists'              , // TODO: Review
+	'LBL_OK'                       => 'OK'                          , // TODO: Review
+	'LBL_EXTENSION_NOT_COMPATABLE' => 'Extension is not Vtiger Compatable', // TODO: Review
+	'LBL_INVALID_FILE'             => 'Invalid File'                , // TODO: Review
+	'LBL_NO_LICENSE_PROVIDED'      => 'No License Provided'         , // TODO: Review
+	'LBL_INSTALLATION'             => 'Installation'                , // TODO: Review
+	'LBL_FAILED'                   => 'Failed'                      , // TODO: Review
+	'LBL_SUCCESSFULL'              => 'Successfull'                 , // TODO: Review
+	'LBL_INSTALLATION_LOG'         => 'Installation Log'            , // TODO: Review
+    
+        //Extension Store translations
+        'LBL_VTIGER_EXTENSION_STORE' => 'Vtiger tienda Extensión',
+        'LBL_SEARCH_FOR_EXTENSION'  => 'Búsqueda de Extensión',
+        'LBL_DOWNLOADS' => 'Descargas',
+        'LBL_NO_EXTENSIONS_FOUND' => 'No hay extensiones encontrados',
+        'LBL_REGISTER_USER' => 'Registro de Usuario',	
+        'LBL_SETUP_CARD_DETAILS' => 'Información de la Tarjeta de instalación',
+        'LBL_SETUP_CARD' => 'Tarjeta de configuración',
+        'LBL_CARD_NUMBER' => 'Número de tarjeta',
+        'LBL_EXP_MONTH' => 'Exp Mes',
+        'LBL_EXP_YEAR' => 'Exp Año',
+        'LBL_CVC_CODE' => 'CVC Código de 3 dígitos',
+        'LBL_RESET' => 'Reajustar',
+        'LBL_EXTENSION_STORE' => 'Extensión tienda',
+        'LBL_INSTALLED' => 'Instalado',
+        'LBL_UPDATE_CARD_DETAILS' => 'Detalles de la actualización de tarjetas',
+        'LBL_BY' => 'Por',
+        'LBL_RATINGS' => 'Valoraciones',
+        'LBL_DESCRIPTION' => 'Descripción',
+        'LBL_AUTHOR_INFORMATION' => 'Información del Autor',
+        'LBL_AUTHOR_NAME' => 'Nombre del Autor',
+        'LBL_PHONE' => 'Teléfono',
+        'LBL_EMAIL' => 'Email',
+        'LBL_SCREEN_SHOTS' => 'Imágenes',
+        'LBL_CUSTOMER_RATINGS' => 'Calificaciones de clientes',
+        'LBL_CUSTOMER_REVIEWS' => 'Opiniones de los usuarios',
+        'LBL_WRITE_A_REVIEW' => 'Escriba su opinión',
+        'LBL_CUSTOMER_REVIEW' => 'Valoración de los clientes',
+        'LBL_REVIEW' => 'Revisión',
+        'LBL_CUSTOMERS_REVIEWED' => 'Clientes Opinión',
+        'LBL_SINGLE_CUSTOMER_REVIEWED' => 'Solo al cliente Opinión',
+        'LBL_INSTALLATION_FAILED' => 'Fallo en la Instalación',
+        'LBL_SUCCESSFULL_INSTALLATION' => 'Instalación de éxito',
+        'Install' => 'Instalar',
+        'Upgrade' => 'Modernización',
+        'LBL_RATE_IT' => 'Califica esta foto',
+);
+$jsLanguageStrings = array(
+  'JS_PLEASE_SETUP_CARD_DETAILS_TO_INSTALL_THIS_EXTENSION'  => 'Por favor, detalles de la tarjeta de configuración para instalar esta extensión',
+  'JS_UPDATE_CARD_DETAILS' => 'Detalles de la actualización de tarjetas',
+  'JS_ON' => 'en',
+  'JS_RATINGS' => 'Valoraciones',
+  'JS_PLEASE_INSTALL_EXTENSION_LOADER_TO_INSTALL_THIS_EXTENSION_FROM_BELOW_LINK' => 'Por favor, instala el gestor de extensión para instalar esta extensión de abajo enlace',
+);

4041ee5a3e0b07b3b791c59dd884b60a8ed5cd90
prasad <prasad@83ef7b00-670c-0410-b3e3-e45103aa6900>
2014-10-07 11:43:56 +0000

    Recreating 6.1.0 tag

---
+++ vtigercrm/tags/vtigercrm-6.1.0/languages/en_us/ServiceContracts.php
@@ -0,0 +1,26 @@
+<?php
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * *********************************************************************************** */
+$languageStrings = array(	
+	'Actual Duration' => 'Actual Duration (in Days)',
+	'Contract No' => 'Contract Number',
+	'End Date' => 'End Date',
+	'LBL_ADD_RECORD' => 'Add Service Contract',
+	'LBL_RECORDS_LIST' => 'Service Contracts List',
+	'LBL_SERVICE_CONTRACT_INFORMATION' => 'Service Contract Details',
+	'Planned Duration' => 'Planned Duration (in Days)',
+	'Progress'=> 'Progress',
+	'Service Contracts' => 'Service Contracts',
+	'ServiceContracts' => 'Service Contracts',
+	'SINGLE_ServiceContracts' => 'Service Contract',
+	'Start Date' => 'Start Date',
+	'Total Units' => 'Total Units',
+	'Tracking Unit' => 'Tracking Unit',
+	'Used Units' => 'Used Units',
+);
